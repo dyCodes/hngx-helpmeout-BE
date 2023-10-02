f4eb95ddce1b8067e095077ef823e5a13f6e20f0
dyCodes <yusufdaudu51@gmail.com>
2023-10-02 03:07:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { getAllVideos, getVideo, saveVideo } = require("./controllers/video.controller");
+require("dotenv/config");
+
+// Connect to DB
+mongoose.connect(process.env.MONGODB_CONNECTION, { useNewUrlParser: true });
+mongoose.connection.on("connected", () => console.log("Connected to database"));
+mongoose.connection.on("error", (err) => console.error(err));
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => res.send("HNGx Stage 5 task [dyCodes]"));
+
+// Export app
+module.exports = app;
